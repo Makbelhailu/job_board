bceabfb87f87984d8f745a6836872794e1a18cbe
Makbelhailu <mcwelson92@gmail.com>
2024-05-11 01:09:47 +0300

    still on progress

--- client/src/components/cards.jsx
+++ client/src/components/cards.jsx
@@ -11,17 +11,17 @@ export const JobCard = ({ content }) => {
         <CardContent className="px-3 hover:bg-secondary hover:text-red-100">
           <div className="my-5 flex h-full w-full gap-x-4 ">
             <Avatar src={content.image} alt="user icon" />
-            <div className="leading-none">
-              <h3>{content.username}</h3>
-              <h4>{content.location}</h4>
+            <div className="text-left leading-none">
+              <h3 className="text-sm font-semibold">{content.username}</h3>
+              <h4 className="text-[10px]">{content.location}</h4>
             </div>
           </div>
           <div className="text-left">
             <h2>{content.title}</h2>
             <p>{content.description}</p>
             <div className="flex items-center justify-start gap-x-3 text-center">
-              <div className="bg-blueish-100 px-2 py-2 text-xs text-blueish">
-                hi
+              <div className="bg-blueish-100 rounded-sm px-4 py-1 text-xs text-blueish">
+                2 Positions
               </div>
               <div className=" px-2 py-2 text-xs ">bye</div>
               <div className=" px-2 py-2 text-xs ">see you</div>

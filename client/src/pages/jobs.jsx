424b3a993474d0a4fe7f8c0cce2f5517a5e65864
Makbelhailu <104273454+Makbelhailu@users.noreply.github.com>
2024-07-03 15:12:32 +0000

    rush commit

--- client/src/pages/jobs.jsx
+++ client/src/pages/jobs.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import MyButton from "../components/button";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
 
 const Jobs = () => {
   return (
-    <div className='h-screen w-80 sticky left-0 bg-secondary'>Jobs</div>
-  )
-}
+    <div className="sticky left-0 h-screen w-80 ">
+      <div className="fillter flex items-center justify-around">
+        <div className="text text-md font-bold">Filter Jobs</div>
+        <MyButton
+          className="mb-2 rounded-md p-0 text-xs font-bold"
+          colored={true}
+        >
+          Clear All
+        </MyButton>
+      </div>
+      <Accordion defaultExpanded>
+        <AccordionSummary id="panel-header" aria-controls="panel-content">
+          Header
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary id="panel-header" aria-controls="panel-content">
+          Header
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
 
-export default Jobs
+export default Jobs;

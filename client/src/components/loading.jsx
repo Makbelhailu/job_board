81fe14b4c7dff503fffa33543f05d4487c97b5f0
Makbelhailu <mcwelson92@gmail.com>
2024-08-04 09:28:35 +0000

    Aug 4, 2024, 11:28 AM

--- client/src/components/loading.jsx
+++ client/src/components/loading.jsx
@@ -5,7 +5,7 @@ import "ldrs/grid";
 
 const Loading = () => {
   return (
-    <div className="grid h-full w-full place-items-center">
+    <div className="grid h-full w-full place-items-center place-self-center">
       <l-grid size="250" speed="1.5" color="#571cbd"></l-grid>
     </div>
   );

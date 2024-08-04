8c21e3d8714929a49c882dc908ebdfe65f30d137
Makbelhailu <mcwelson92@gmail.com>
2024-08-04 09:43:25 +0000

    Aug 4, 2024, 11:43 AM

--- client/src/components/loading.jsx
+++ client/src/components/loading.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import "ldrs/newtonsCradle";
+import "ldrs/bouncy";
 
 // Default values shown
 
 const Loading = () => {
   return (
     <div className="grid h-full w-full place-items-center place-self-center">
-      <l-newtons-cradle
-        size="250"
-        speed="1.5"
-        color="#571cbd"
-      ></l-newtons-cradle>
+      <l-bouncy size="150" speed="1.5" color="#571cbd"></l-bouncy>
     </div>
   );
 };

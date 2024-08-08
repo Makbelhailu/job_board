55735b25dcc3cdb9fda711a56e8deae5acee1a1d
Makbelhailu <mcwelson92@gmail.com>
2024-08-08 19:43:35 +0000

    Aug 8, 2024, 9:43 PM

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -22,6 +22,8 @@ const getAllJobs = async (req, res) => {
 
     const companyIds = jobList.map((job) => job.companyId);
     const companyInfo = await User.find({ userId: { $in: companyIds } });
+
+    const fullList = jobList.reduce();
     res.status(200).json(jobList);
     console.log("all jobs fetched");
   } catch (err) {

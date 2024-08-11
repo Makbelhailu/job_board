88547a1da8957cbda1780c92f55b094f5d2ac34e
Makbelhailu <mcwelson92@gmail.com>
2024-08-11 10:26:56 +0000

    Aug 11, 2024, 12:26 PM

--- client/src/components/button.jsx
+++ client/src/components/button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "classnames";
 
 const MyButton = ({ children, colored, className, onClick }) => {
-  const defaultClasses = `bg-inherit rounded-full border-2  ${colored ? `border-secondary text-secondary hover:text-white` : `hover:border-secondary border-slate-500`} mx-2 px-7 py-2 hover:bg-secondary hover:text-white`;
+  const defaultClasses = `bg-inherit rounded-full border-2 shadow-lg ${colored ? `border-secondary text-secondary hover:text-white` : `hover:border-secondary border-slate-500`} mx-2 px-7 py-2 hover:bg-secondary hover:text-white`;
   const combinedClasses = classNames(defaultClasses, className);
   return (
     <div className={combinedClasses} onClick={onClick}>
